9cfb5b51d1270838640c81856c5b7b85a429059e
Rui Azevedo <ruiazevedodev@outlook.com>
2023-06-09 18:22:54 +0100

    data changed

--- src/data.jsx
+++ src/data.jsx
@@ -14,7 +14,7 @@ export default [
     },
     {
         id: 2,
-        title: "Learn Wedding Photography with Professional",
+        title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
         coverImg: "wedding-photography.png",
@@ -27,7 +27,7 @@ export default [
     },
     {
         id: 3,
-        title: "Group Mountain Biking on pros bikes",
+        title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
         coverImg: "mountain-bike.png",
